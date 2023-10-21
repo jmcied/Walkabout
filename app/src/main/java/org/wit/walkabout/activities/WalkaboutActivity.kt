56fe0535ae87a214cdab3974e2a5d7bf1bbd3d8a
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-10-21 18:15:30 +0100

    Added walk Arraylist, added description,difficulty, terrain fields

--- app/src/main/java/org/wit/walkabout/activities/WalkaboutActivity.kt
+++ app/src/main/java/org/wit/walkabout/activities/WalkaboutActivity.kt
@@ -12,19 +12,26 @@ class WalkaboutActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWalkaboutBinding
     var walk = WalkaboutModel()
+    val walks = ArrayList<WalkaboutModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityWalkaboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         Timber.plant(Timber.DebugTree())
         i("Placemark Activity started...")
 
         binding.btnAdd.setOnClickListener() {
             walk.title = binding.walkTitle.text.toString()
+            walk.description = binding.description.text.toString()
+            walk.difficulty = binding.difficulty.text.toString()
+            walk.terrain = binding.terrain.text.toString()
             if (walk.title.isNotEmpty()) {
-                i("add Button Pressed: ${walk.title}")
+                walks.add(walk.copy())
+                i("add Button Pressed: ${walk}")
+                for (i in walks.indices) {
+                    i("Walk[$i]:${this.walks[i]}")
+                }
             } else {
                 Snackbar
                     .make(it, "Please Enter a title", Snackbar.LENGTH_LONG)
--- app/src/main/java/org/wit/walkabout/models/WalkaboutModel.kt
+++ app/src/main/java/org/wit/walkabout/models/WalkaboutModel.kt
@@ -1,3 +1,6 @@
 package org.wit.walkabout.models
 
-data class WalkaboutModel(var title: String = "")
+data class WalkaboutModel(var title: String = "",
+                        var description: String = "",
+                        var difficulty: String = "",
+                        var terrain: String = "")
