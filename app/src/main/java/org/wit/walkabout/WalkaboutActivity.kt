6824e477dac87067676f347617c9f58259a704f2
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-10-21 16:34:32 +0100

    Event Logging

--- app/src/main/java/org/wit/walkabout/WalkaboutActivity.kt
+++ app/src/main/java/org/wit/walkabout/WalkaboutActivity.kt
@@ -2,10 +2,15 @@ package org.wit.walkabout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.i
 
 class WalkaboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_walkabout)
+
+        Timber.plant(Timber.DebugTree())
+        i("Placemark Activity started...")
     }
 }
