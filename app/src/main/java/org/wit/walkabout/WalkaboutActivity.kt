4f921a535198757515eb7337929f20532552ea41
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-10-21 17:29:48 +0100

    Event Handling

--- app/src/main/java/org/wit/walkabout/WalkaboutActivity.kt
+++ app/src/main/java/org/wit/walkabout/WalkaboutActivity.kt
@@ -1,16 +1,35 @@
 package org.wit.walkabout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import org.wit.walkabout.databinding.ActivityWalkaboutBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
+
+
 class WalkaboutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWalkaboutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_walkabout)
+
+        binding = ActivityWalkaboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
         i("Placemark Activity started...")
+
+        binding.btnAdd.setOnClickListener() {
+            val walkTitle = binding.walkTitle.text.toString()
+            if (walkTitle.isNotEmpty()) {
+                i("add Button Pressed: $walkTitle")
+            } else {
+                Snackbar
+                    .make(it, "Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 }
