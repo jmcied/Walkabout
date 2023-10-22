f43ab29189e9c6f10e5ccbd76e187d4aa9618e48
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-10-22 13:30:13 +0100

    Card + RecyclerView

--- app/src/main/java/org/wit/walkabout/activities/WalkaboutListActivity.kt
+++ app/src/main/java/org/wit/walkabout/activities/WalkaboutListActivity.kt
@@ -1,16 +1,59 @@
 package org.wit.walkabout.activities
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import org.wit.walkabout.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.wit.walkabout.databinding.ActivityWalkaboutListBinding
+import org.wit.walkabout.databinding.CardWalkaboutBinding
 import org.wit.walkabout.main.MainApp
+import org.wit.walkabout.models.WalkaboutModel
 
 class WalkaboutListActivity : AppCompatActivity() {
 
     lateinit var app: MainApp
+    private lateinit var binding: ActivityWalkaboutListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_walkabout_list)
+        binding = ActivityWalkaboutListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = WalkaboutAdapter(app.walks)
+
+    }
+}
+
+class WalkaboutAdapter constructor(private var walks: List<WalkaboutModel>) :
+    RecyclerView.Adapter<WalkaboutAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardWalkaboutBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val walk = walks[holder.adapterPosition]
+        holder.bind(walk)
+    }
+
+    override fun getItemCount(): Int = walks.size
+
+    class MainHolder(private val binding : CardWalkaboutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(walk: WalkaboutModel) {
+            binding.walkTitle.text = walk.title
+            binding.description.text = walk.description
+            binding.difficulty.text = walk.difficulty
+            binding.terrain.text = walk.terrain
+        }
     }
 }
