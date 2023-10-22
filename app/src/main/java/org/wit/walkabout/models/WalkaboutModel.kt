02e8a758db75ddc258ea64bf3aeea8f63a4fd869
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-10-22 17:04:15 +0100

    Passing Data - Parcelable

--- app/src/main/java/org/wit/walkabout/models/WalkaboutModel.kt
+++ app/src/main/java/org/wit/walkabout/models/WalkaboutModel.kt
@@ -1,6 +1,10 @@
 package org.wit.walkabout.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class WalkaboutModel(var title: String = "",
                         var description: String = "",
                         var difficulty: String = "",
-                        var terrain: String = "")
+                        var terrain: String = "") : Parcelable
