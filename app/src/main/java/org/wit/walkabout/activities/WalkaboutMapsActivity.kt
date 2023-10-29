b3a27fbd03a0135613bd005469aa4495e0840e85
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-10-29 17:46:09 +0000

    content layouts

--- app/src/main/java/org/wit/walkabout/activities/WalkaboutMapsActivity.kt
+++ app/src/main/java/org/wit/walkabout/activities/WalkaboutMapsActivity.kt
@@ -2,19 +2,48 @@ package org.wit.walkabout.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
 import org.wit.walkabout.databinding.ActivityWalkaboutMapsBinding
+import org.wit.walkabout.databinding.ContentWalkaboutMapsBinding
 
 class WalkaboutMapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWalkaboutMapsBinding
-
+    private lateinit var contentBinding: ContentWalkaboutMapsBinding
+    lateinit var map: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityWalkaboutMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
+        contentBinding = ContentWalkaboutMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState)
+
         }
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
     }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
+    }
+}
