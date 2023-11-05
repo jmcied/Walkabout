216107a55bbcd627377951d4d8adbe60b70b6c7b
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-11-05 15:14:30 +0000

    User RegisterActivity

---
+++ app/src/main/java/org/wit/walkabout/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package org.wit.walkabout.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.wit.walkabout.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
